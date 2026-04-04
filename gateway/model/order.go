6ba197baeabc4b3f998f1ae806e0dde466f4a4e8
Airlangga <airlanggakrishna@gmail.com>
2026-04-04 18:22:01 +0700

    chore: json tag

--- gateway/model/order.go
+++ gateway/model/order.go
@@ -3,10 +3,10 @@ package model
 import "github.com/google/uuid"
 
 type Order struct {
-	OrderID           uuid.UUID
-	RestaurantName    string
-	DriverName        string
-	ItemName          string
-	RestaurantAddress string
-	DeliveryAddress   string
+	OrderID           uuid.UUID `json:"order_id"`
+	RestaurantName    string    `json:"restaurant_name"`
+	DriverName        string    `json:"driver_name"`
+	ItemName          string    `json:"item_name"`
+	RestaurantAddress string    `json:"restaurant_address"`
+	DeliveryAddress   string    `json:"delivery_address"`
 }
