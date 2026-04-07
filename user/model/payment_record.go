dce1bd57150b0cafe13bcdfeb0b27fe86b9967cb
Airlangga <airlanggakrishna@gmail.com>
2026-04-08 03:05:04 +0700

    chore: change field type

--- user/model/payment_record.go
+++ user/model/payment_record.go
@@ -17,9 +17,9 @@ const (
 )
 
 type PaymentRecord struct {
-	ID                     bson.ObjectID `bson:"_id,omitempty"`
-	Email                  string        `bson:"email"`
-	EmailStatus            EmailStatus   `bson:"email_status"`
-	PaymentType            PaymentType   `bson:"payment_type"`
-	PaymentGatewayResponse any           `bson:"payment_gateway_response"`
+	ID                     bson.ObjectID          `bson:"_id,omitempty"`
+	Email                  string                 `bson:"email"`
+	EmailStatus            EmailStatus            `bson:"email_status"`
+	PaymentType            PaymentType            `bson:"payment_type"`
+	PaymentGatewayResponse PaymentGatewayResponse `bson:"payment_gateway_response"`
 }
