86a4582e4c8192a0312ae29aa444444bdb27d781
Airlangga <airlanggakrishna@gmail.com>
2026-04-07 14:11:52 +0700

    chore: customerRepository struct

--- order/repository/customer.go
+++ order/repository/customer.go
@@ -1,1 +1,11 @@
 package repository
+
+import "database/sql"
+
+type customerRepository struct {
+	db *sql.DB
+}
+
+func New(db *sql.DB) *customerRepository {
+	return &customerRepository{db: db}
+}
