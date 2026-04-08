1c654c09b822280f933ec146e1220896f26fe968
Firmeteran <mfirmansyah12345@gmail.com>
2026-04-08 17:58:03 +0700

    user: repo (p3)

--- user/repository/user.go
+++ user/repository/user.go
@@ -10,7 +10,7 @@ import (
 type UserRepository interface {
 	Register(ctx context.Context, user model.UserRegister) error
 	Login(ctx context.Context, email string) (model.UserInfo, string, error)
-	FindUserByEmail(ctx context.Context, email string) (model.UserInfo, string, error)
+	GetUserByInfo(ctx context.Context, email string) (model.UserInfo, string, error)
 }
 
 type userRepository struct {
@@ -21,6 +21,19 @@ func NewUserRepository(db *sql.DB) UserRepository {
 	return &userRepository{db}
 }
 
-func (r *userRepository) Register(ctx context.Context, user model.UserRegister) error {
+func (r *userRepository) Register(ctx context.Context, u model.UserRegister) error {
+	query := `INSERT INTO users (first_name, last_name, email, password, address) VALUES ($1, $2, $3, $4, $5)`
+	_, err := r.db.ExecContext(ctx, query, u.FirstName, u.LastName, u.Email, u.Password, u.Address)
+	return err
+}
 
+func (r *userRepository) Login(ctx context.Context, email string) (model.UserInfo, string, error) {
+	var u model.UserInfo
+	var password string
+	query := `SELECT first_name, last_name, email, password, address, balance FROM users WHERE = $1`
+
+	err := r.db.QueryRowContext(ctx, query, email).Scan(
+		&u.FirstName, &u.LastName, &u.Email, &u.Address, &u.Balance, &password,
+	)
+	return u, password, err
 }
