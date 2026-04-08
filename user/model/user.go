2b3e720c807a5778bc6dac6170089dd863b35022
Firmeteran <mfirmansyah12345@gmail.com>
2026-04-08 17:26:16 +0700

    user: models init

---
+++ user/model/user.go
@@ -0,0 +1,21 @@
+package model
+
+type UserRegister struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	Address   string `json:"address"`
+}
+
+type UserInfo struct {
+	FirstName string  `json:"first_name"`
+	LastName  string  `json:"last_name"`
+	Email     string  `json:"email"`
+	Address   string  `json:"address"`
+	Balance   int     `json:"balance"`
+}
+
+type PaymentLink struct {
+	PaymentLink string `json:"payment_link"`
+}
