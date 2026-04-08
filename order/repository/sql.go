33ae9f5f90dee3bd3a9eac51856150481d473ef5
Airlangga <airlanggakrishna@gmail.com>
2026-04-08 19:47:43 +0700

    feat: get orders by user id

--- order/repository/sql.go
+++ order/repository/sql.go
@@ -239,6 +239,123 @@ func (r *sqlRepository) GetOrderByOrderID(ctx context.Context, tx *sql.Tx, order
 	return resultOrder, nil
 }
 
+func (r *sqlRepository) GetOrdersByUserID(ctx context.Context, userID uuid.UUID, role string) ([]model.Order, error) {
+	var filterCol string
+	switch role {
+	case "customer":
+		filterCol = "customer_id"
+	case "driver":
+		filterCol = "driver_id"
+	default:
+		return nil, fmt.Errorf("invalid role: %s: %w", role, model.ErrNotFound)
+	}
+
+	query := fmt.Sprintf(`
+		SELECT
+			cp.address,
+			cp.phone_number,
+			r.id,
+			r.name,
+			r.address,
+			i.id,
+			i.name,
+			i.price,
+			oi.quantity,
+			o.order_status,
+			o.delivery_fee,
+			o.total_fee,
+			o.id
+		FROM
+			orders o
+		JOIN
+			customer_profiles cp ON o.customer_id = cp.user_id
+		JOIN
+			order_items oi ON o.id = oi.order_id
+		JOIN
+			items i ON oi.item_id = i.id
+		JOIN
+			restaurants r ON i.restaurant_id = r.id
+		WHERE
+			o.%s = $1`, filterCol)
+
+	rows, err := r.db.QueryContext(ctx, query, userID)
+	if err != nil {
+		return nil, fmt.Errorf("order.customer_repo.GetOrdersByUserID (tx.QueryContext): %w", err)
+	}
+	defer rows.Close()
+
+	ordersMap := make(map[uuid.UUID]*model.Order)
+
+	orderRestoMap := make(map[uuid.UUID]map[uuid.UUID]*model.Restaurant)
+
+	for rows.Next() {
+		var restoName, restoAddr, itemName, deliveryAddr, customerPhoneNumber, orderStatus string
+		var restoID, itemID, orderID uuid.UUID
+		var itemPrice, itemQty, deliveryFee, totalFee int
+
+		err := rows.Scan(
+			&deliveryAddr,
+			&customerPhoneNumber,
+			&restoID,
+			&restoName,
+			&restoAddr,
+			&itemID,
+			&itemName,
+			&itemPrice,
+			&itemQty,
+			&orderStatus,
+			&deliveryFee,
+			&totalFee,
+			&orderID,
+		)
+		if err != nil {
+			return nil, fmt.Errorf("order.customer_repo.GetOrdersByUserID (rows.Scan): %w", err)
+		}
+
+		if _, ok := ordersMap[orderID]; !ok {
+			ordersMap[orderID] = &model.Order{
+				ID:                  orderID,
+				DeliveryAddress:     deliveryAddr,
+				CustomerPhoneNumber: customerPhoneNumber,
+				OrderStatus:         model.OrderStatus(orderStatus),
+				DeliveryFee:         deliveryFee,
+				TotalFee:            totalFee,
+				Restaurants:         []model.Restaurant{},
+			}
+			orderRestoMap[orderID] = make(map[uuid.UUID]*model.Restaurant)
+		}
+
+		restosInOrder := orderRestoMap[orderID]
+
+		if _, ok := restosInOrder[restoID]; !ok {
+			newResto := &model.Restaurant{
+				ID:      restoID,
+				Name:    restoName,
+				Address: restoAddr,
+				Items:   []model.Item{},
+			}
+			restosInOrder[restoID] = newResto
+		}
+
+		restosInOrder[restoID].Items = append(restosInOrder[restoID].Items, model.Item{
+			ID:       itemID,
+			Name:     itemName,
+			Price:    itemPrice,
+			Quantity: itemQty,
+		})
+	}
+
+	result := make([]model.Order, 0, len(ordersMap))
+	for orderID, o := range ordersMap {
+		for _, r := range orderRestoMap[orderID] {
+			o.Restaurants = append(o.Restaurants, *r)
+		}
+		result = append(result, *o)
+	}
+
+	return result, nil
+}
+
 func (r *sqlRepository) GetDrivers(ctx context.Context, orderID uuid.UUID) ([]model.Driver, error) {
 	rows, err := r.db.QueryContext(
 		ctx,
@@ -341,12 +458,12 @@ func (r *sqlRepository) ChooseDriver(ctx context.Context, orderID, driverID uuid
 	}); err != nil {
 		return model.Order{}, fmt.Errorf("order.respotiry.ChooseDriver (UpdateOrder): %w", err)
 	}
-	
+
 	order, err := r.GetOrderByOrderID(ctx, tx, orderID)
 	if err != nil {
 		return model.Order{}, fmt.Errorf("order.respotiry.ChooseDriver (GetOrderByOrderID): %w", err)
 	}
-	
+
 	return order, tx.Commit()
 }
 
